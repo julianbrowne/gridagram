ee89b1bf7f49f9a3cb86eabaa7e40d137c4e4462
Julian Browne <julianbrowne@gmail.com>
2013-09-08 11:41:22 +0100

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,38 @@ thegrid
 =======
 
 Manages dynamic grid layout of on screen data like a poor man's gridster
+
+    <script src="thegrid.js"></script>
+    <link rel="stylesheet" href="thegrid.css" />
+
+    ...
+
+    var grid = new Grid("#results");                    // target element
+    grid.autoHeight = true;                             // align heights of all widget elements
+
+    grid.empty();                                       // clears target element
+
+    var w1 = new grid.Widget("widget 1");               // make a new widget
+
+    var w2 = new grid.Widget("widget 2");
+    w2.autoStretch = true;                              // grow this widget height with its content (no scroll)
+
+    for(var i=0; i<10; i++) { 
+        w1.addKeyValue("key","value" + i);              // add lines of the form "key .... value" with leader dots
+    }
+
+    for(var i=0; i<25; i++) { 
+        w2.addKeyValue("somekey","somevalue-" + i);
+    }
+    w2.addKeyValue("login","pass", {}, { class: "pass" });
+
+    var w3 = new grid.Widget("widget 3");
+
+    w3.addKeyValue("a key","a value");
+    w3.addKeyValue("left","right");
+
+    w1.addLine("something");                            // add regular lines of text
+    w2.addLine("something");
+    w3.addLine("something");
+
+    var w4 = new grid.Widget("widget 4");
---
+++ thegrid.js
@@ -0,0 +1,103 @@
+/**
+ *  The Grid
+**/
+
+function Grid(targetElement) { 
+
+    this.widgets = [];
+    this.root = $(targetElement);
+    this.autoHeight = false;
+    this.widgetDefaults = {
+        height: 100,
+        width: 200
+    };
+    var grid = this;
+
+    grid.root.addClass('thegrid');
+
+    this.Widget = function(title) { 
+        this.title = title;
+        this.index = grid.widgets.length;
+        this.id = "widget-" + this.index;
+        this.width = grid.widgetDefaults.width;
+        this.height = grid.widgetDefaults.height;
+        var widgetHTML = $("<div>", { id: this.id, class: "widget", style: "width: " + this.width + "; " + "height: " + this.height + ";" });
+        var titleHTML  = $("<div>", { class: "title" });
+        grid.root.append(widgetHTML);
+        this.widget = $("#" + this.id);
+        this.widget.append(titleHTML);
+        this.widget.children("div").html(this.title);
+        var testListHTML = $("<ul>", {});
+        this.widget.append(testListHTML);
+        this.list = this.widget.children("ul");
+        this.lines = 0;
+        this.autoStretch = false;
+        grid.widgets.push(this);
+
+        this.css = function(attr, value) { 
+            if(value === undefined) return this.widget.css(attr);
+            this.widget.css(attr, value);
+        };
+
+        this.height = function(height) { 
+            if(height === undefined) return parseInt(this.css("height"));
+            this.widget.css("height", height);
+        };
+
+
+        this.addLine = function(text) {
+            var id = (this.lines++);
+            var lineId = "line-item-" + this.index + "-" + id;
+            var lineSelector = "#" + lineId;
+            var listItem = $("<li>",   { id: lineId, class: "line" });
+            this.list.append(listItem);
+            if(text !== undefined) $(lineSelector).append(text);
+            if(this.autoStretch) this.stretchToFit();
+            return $(lineSelector);
+        };
+
+        this.addKeyValue = function(key, value, keyAttrs, valueAttrs) { 
+            var keyAttrs = (keyAttrs === undefined) ? {} : keyAttrs;
+            var valueAttrs = (valueAttrs === undefined) ? {} : valueAttrs;
+            this.widget.children("ul").addClass("leaders");
+            var line = this.addLine();
+            keyAttrs = $.extend({}, keyAttrs);
+            keyAttrs.class = "key " + keyAttrs.class;
+            var keySpan = $("<span>", keyAttrs).html(key);
+            valueAttrs = $.extend({}, valueAttrs);
+            valueAttrs.class = "value " + valueAttrs.class;
+            var valSpan = $("<span>", valueAttrs).html(value);
+            line.append(keySpan).append(valSpan);
+            return line;
+        };
+
+        this.applyHeights = function(height) { 
+            grid.widgets.forEach( 
+                function(widget) { 
+                    widget.height(height);
+                }
+            );            
+        };
+
+        this.stretchToFit = function() { 
+            while(this.hasVerticalScrollBar() === true) { 
+                var newHeight = this.height() + 5;
+                this.height(newHeight);
+            }
+            if(grid.autoHeight) { 
+                this.applyHeights(newHeight);
+                grid.widgetDefaults.height = newHeight;
+            }
+        };
+
+        this.hasVerticalScrollBar = function() { 
+            return ((this.widget[0].clientHeight < this.widget[0].scrollHeight)); // || (this.widget[0].clientWidth < this.widget[0].scrollWidth));
+        };
+
+    };
+
+    this.empty = function() { 
+        this.root.empty();
+    };
+
+};
