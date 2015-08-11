4de26acd48ea1d964eefdb0783c2170a7a355078
julianbrowne <julian@webskill.com>
2015-08-11 14:29:07 +0100

    test tidy

--- test/spec/gridagram.js
+++ test/spec/gridagram.js
@@ -56,7 +56,34 @@ describe("Gridagram", function() {
             $("#test").gridagram(data);
             expect($(".gridagram").length).toEqual(1);
             expect($(".widget").length).toEqual(2);
-            //helper.removeTestContainer("test");
+            helper.removeTestContainer("test");
+        });
+
+        it("should order contents", function() { 
+            helper.addTestContainer("test");
+            var data = [ 
+                {title: "a", body: "body"}, 
+                {title: "f", body: "body"}, 
+                {title: "c", body: "body"}, 
+                {title: "d", body: "body"}, 
+                {title: "g", body: "body"}, 
+                {title: "b", body: "body"}, 
+                {title: "e", body: "body"}, 
+            ];
+            $("#test").gridagram(data, { 
+                widgetId: function(elem) { return elem.title },
+                orderBy: function(a,b) { return (a.title > b.title) - (a.title < b.title) }
+            });
+            expect($(".gridagram").length).toEqual(1);
+            expect($(".widget").length).toEqual(7);
+            expect($($(".widget")[0]).find(".title-text").html()).toEqual("a");
+            expect($($(".widget")[1]).find(".title-text").html()).toEqual("b");
+            expect($($(".widget")[2]).find(".title-text").html()).toEqual("c");
+            expect($($(".widget")[3]).find(".title-text").html()).toEqual("d");
+            expect($($(".widget")[4]).find(".title-text").html()).toEqual("e");
+            expect($($(".widget")[5]).find(".title-text").html()).toEqual("f");
+            expect($($(".widget")[6]).find(".title-text").html()).toEqual("g");
+            helper.removeTestContainer("test");
         });
 
     });
